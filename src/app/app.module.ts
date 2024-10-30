559c4b4e78777eb78e64fe604a0855d740af182b
m0dchi <152161177+m0dchi@users.noreply.github.com>
2024-10-30 20:33:38 -0300

    update code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,4 +24,5 @@ import { FormsModule } from '@angular/forms';
   ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- src/app/services/test.service.ts
+++ src/app/services/test.service.ts
@@ -2,17 +2,25 @@ import { Injectable } from "@angular/core"
 import { Observable, ObservedValuesFromArray } from "rxjs"
 import { HttpClient } from "@angular/common/http"
 import { Phone } from "../models/phones"
+import { map } from "rxjs/operators"
 
 @Injectable({
   providedIn: "root"
 })
 export class TestService {
   private url = "https://api.restful-api.dev/objects"
+  private localPhones = new Map<string, Phone>()
 
   constructor(private http: HttpClient) { }
 
   getAll(): Observable<any> {
-    return this.http.get(this.url);
+    return this.http.get(this.url).pipe(
+      map((response: any) => {
+        const apiPhones = response as Phone[];
+        const localPhonesArray = Array.from(this.localPhones.values());
+        return [...apiPhones, ...localPhonesArray];
+      })
+    );
   }
 
   save(phone: Phone): Observable<any> {
